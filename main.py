dd10de17acea136b8f13786f92ee5adc5e2bf224
Eliya Farhat <91684882+EliyaFarhat@users.noreply.github.com>
2023-01-19 19:40:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,82 @@
-'''
-x = -321
-s = str(x)
-i = list(s[::-1])
-print(i)
-run = True
-for x in range(0, len(i)-1):
+client_id = ""
+client_secret = ""
+import requests
+import json
+import spotipy
+searchURL = "https://api.spotify.com/v1/search"
+from pprint import pprint
+scopes = 'playlist-modify-public'
+youtube_key = ""
+playlist_id = ""
+import googleapiclient
+from googleapiclient.discovery import build
+import time
+def getYouTubePlaylist():
+    # Get Video titles in the playlist
+    youtube = build('youtube', 'v3', developerKey=youtube_key)
+    playlist_id = ""
+    response = youtube.playlistItems().list(part="snippet",
+                                            playlistId=playlist_id,
+                                            maxResults=50).execute()
 
-    if i[-1] == "-":
-        i = i[-1:] + i[:-1]
-    if i[0] == "0":
-        while run == True:
-            print("while", i)
-            if x < len(i):
-                if i[x] == "0" and i[x + 1] == "0":
-                    i[x] = "_"
-                if i[x+1] != "0":
-                    i[x] = "_"
-                    run = False
-                x += 1
-print(i)
-#print(i)
-finalList = list(filter(lambda a: a != "_", i))
+    playlistItems = response['items']
+    nextPageToken = response.get('nextPageToken')
 
-final =""
-for y in range(len(finalList)):
-    final += finalList[y]
+    while nextPageToken:
+        response = youtube.playlistItems().list(part="snippet",
+                                                playlistId=playlist_id,
+                                                maxResults=50).execute()
+        playlistItems.extend(response['items'])
+        nextPageToken = response.get('nextPageToken')
 
-finalRes = int(final)
-if finalRes < -2**31 or finalRes > (2**31) - 1:
-    print(0)
-print(finalRes)
+    res = []
 
-'''
+    for x in range(len(playlistItems)):
+        res.append(playlistItems[x]['snippet']['title'])
 
-s = "hello (Offical Audio)"
-q = "hello (Offical Video)"
-print(s[::-1])
-print(q[::-1])
-r = (q[::-1])[16::][::-1]
-print(r)
+    return res
+def searchTitleGetURL(title):
+
+    sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id=client_id,
+                                                           client_secret=client_secret,
+                                                           redirect_uri="https://github.com/EliyaFarhat", scope=scopes))
+    uriLst = []
+    checkUri = []
+    # Search for songs and get URI's
+    try:
+        check = sp.playlist_items(playlist_id=playlist_id)['items']
+        for y in range(len(check)):
+            checkUri.append(check[y]['track']['uri'])
+        for x in range(len(title)):
+            if ((title[x])[::-1])[:16] == ")oiduA laciffO(" or ((title[x])[::-1])[:16] == ")oediV laciffO(" :
+                title[x] = ((title[x])[::-1])[16::][::-1]
+            if title[x] == "bladee - egobaby (Official Video)":
+                title[x] = "bladee - egobaby"
+            if title[x] == "XXXTENTACION - Everybody Dies In Their Nightmares (Audio)":
+                title[x] = "XXXTENTACION - Everybody Dies In Their Nightmares"
+            searchResults = sp.search(q=title[x], type="track", market="US", limit=1)
+
+            if searchResults['tracks']['items'][0]['uri'] not in checkUri:
+                uriLst.append(searchResults['tracks']['items'][0]['uri'])
+            else:
+                pass
+
+
+        try:
+            sp.playlist_add_items(playlist_id=playlist_id, items=uriLst, position=None)
+        except spotipy.exceptions.SpotifyException:
+            print("NO SONGS OR NEW SONGS FOUND")
+    except spotipy.SpotifyOauthError as e:
+        sp = spotipy.Spotify(auth_manager=spotipy.SpotifyOAuth(client_id="",
+                                                               client_secret="",
+                                                               redirect_uri="https://github.com/EliyaFarhat", scope=scopes))
+
+
+
+while True:
+    searchTitleGetURL(getYouTubePlaylist())
+    time.sleep(1)
+
+
+
+
